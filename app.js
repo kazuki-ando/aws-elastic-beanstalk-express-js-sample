5f9ce802a97b9afeeeb01b8b0221e11768ce56a9
kazuki-ando <wanomichi@icloud.com>
2025-04-28 18:51:24 +0900

    Add Stage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Kazuki!'));
+app.get('/', (req, res) => res.send('Hello Kazuki,Add Next Stage!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
